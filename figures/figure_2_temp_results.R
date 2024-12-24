f26dfe4370bd4935c27d4b626f02a580d6b85cc3
Sean Anderson <sean@seananderson.ca>
2024-12-23 16:15:04 -0800

    Style R file

--- figures/figure_2_temp_results.R
+++ figures/figure_2_temp_results.R
@@ -8,13 +8,15 @@ library(scales)
 library(patchwork)
 library(cowplot)
 
-df = readRDS(file = "output/temp_model_df.rds")
+df <- readRDS(file = "output/temp_model_df.rds")
 
 haul <- readRDS("data/haul_cleaned.rds")
 haul$date_formatted <- as.Date(as.numeric(haul$date_formatted), origin = "1970-01-01")
 haul$year <- lubridate::year(haul$date_formatted)
-haul <- dplyr::filter(haul, year==2018,
-                          !is.na(temperature_at_gear_c_der))
+haul <- dplyr::filter(
+  haul, year == 2018,
+  !is.na(temperature_at_gear_c_der)
+)
 
 # plot predictive in-sample density
 df_train <- dplyr::select(df, -dens_ll_test) |>
@@ -29,18 +31,18 @@ df_train$dens_ll <- df_train$dens_ll / 590
 df_test$dens_ll <- df_test$dens_ll / 65.5
 
 df_plot <- rbind(df_train, df_test) |>
-  dplyr::filter(converged == TRUE, n < 655, cutoff <= 300) 
+  dplyr::filter(converged == TRUE, n < 655, cutoff <= 300)
 
 
 # Individual plots with explicit white backgrounds
-p2a <- rbind(df_train, df_test) |> 
-  dplyr::filter(converged == TRUE, n < 655, cutoff <= 300) |> 
+p2a <- rbind(df_train, df_test) |>
+  dplyr::filter(converged == TRUE, n < 655, cutoff <= 300) |>
   ggplot(aes(cutoff, dens_ll, group = name, col = name)) +
   geom_line(linewidth = 1.5, alpha = 0.8) +
-  xlab("Cutoff distance (km)") + 
-  ylab("Log density") + 
+  xlab("Cutoff distance (km)") +
+  ylab("Log density") +
   scale_color_viridis_d(option = "magma", begin = 0.2, end = 0.8, name = "Data") +
-  theme_bw() + 
+  theme_bw() +
   theme(
     strip.background = element_rect(fill = "white"),
     plot.background = element_rect(fill = "white", color = NA),
@@ -48,14 +50,14 @@ p2a <- rbind(df_train, df_test) |>
     axis.title.y = element_blank()
   )
 
-p2b <- rbind(df_train, df_test) |> 
-  dplyr::filter(converged == TRUE, n < 655, cutoff <= 300) |> 
+p2b <- rbind(df_train, df_test) |>
+  dplyr::filter(converged == TRUE, n < 655, cutoff <= 300) |>
   ggplot(aes(n, dens_ll, group = name, col = name)) +
   geom_line(linewidth = 1.5, alpha = 0.8) +
-  xlab("Mesh vertices (n)") + 
-  ylab("Log density") + 
+  xlab("Mesh vertices (n)") +
+  ylab("Log density") +
   scale_color_viridis_d(option = "magma", begin = 0.2, end = 0.8, name = "Data") +
-  theme_bw() + 
+  theme_bw() +
   theme(
     strip.background = element_rect(fill = "white"),
     plot.background = element_rect(fill = "white", color = NA),
@@ -99,26 +101,22 @@ g <- cowplot::plot_grid(
 ggsave(plot = g, filename = "figures/Figure_02.png", width = 7, height = 6, bg = "white")
 
 
-# Make 2nd plot of parameters 
+# Make 2nd plot of parameters
 all_df <- readRDS(file = "output/temp_model_all_est.rds")
-all_df$term[which(all_df$term=="(Intercept)")] <- "Intercept"
-all_df$term[which(all_df$term=="I(zday^2)")] <- "day2"
-all_df$term[which(all_df$term=="phi")] <- "Obs SD"
-all_df$term[which(all_df$term=="range")] <- "Spatial range"
-all_df$term[which(all_df$term=="sigma_O")] <- "Spatial SD"
-all_df$term[which(all_df$term=="zday")] <- "day"
-all_df$term <- factor(all_df$term, levels = c("Intercept","day","day2","Spatial range","Spatial SD","Obs SD"))
+all_df$term[which(all_df$term == "(Intercept)")] <- "Intercept"
+all_df$term[which(all_df$term == "I(zday^2)")] <- "day2"
+all_df$term[which(all_df$term == "phi")] <- "Obs SD"
+all_df$term[which(all_df$term == "range")] <- "Spatial range"
+all_df$term[which(all_df$term == "sigma_O")] <- "Spatial SD"
+all_df$term[which(all_df$term == "zday")] <- "day"
+all_df$term <- factor(all_df$term, levels = c("Intercept", "day", "day2", "Spatial range", "Spatial SD", "Obs SD"))
 
-ggplot(all_df, aes(cutoff, mean_estimate)) + 
-  geom_ribbon(aes(ymin = mean_estimate - 2*mean_se, ymax = mean_estimate + 2*mean_se), fill=viridis(1),alpha=0.3) + 
-  geom_line(col=viridis(1)) + 
-  facet_wrap(~term, scale="free_y") +
-  xlab("Cutoff distance (km)") + 
-  ylab("Estimate") + 
-  theme_bw() + 
-  theme(strip.background = element_rect(fill="white"))
+ggplot(all_df, aes(cutoff, mean_estimate)) +
+  geom_ribbon(aes(ymin = mean_estimate - 2 * mean_se, ymax = mean_estimate + 2 * mean_se), fill = viridis(1), alpha = 0.3) +
+  geom_line(col = viridis(1)) +
+  facet_wrap(~term, scale = "free_y") +
+  xlab("Cutoff distance (km)") +
+  ylab("Estimate") +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = "white"))
 ggsave(filename = "figures/Figure_02b.png", width = 7, height = 5)
-
-
-
-
