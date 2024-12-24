a4cd37b1208b51220bf03dafc7f4f96f83e41755
Sean Anderson <sean@seananderson.ca>
2024-12-23 16:13:49 -0800

    Tweak fig 1

--- figures/figure_01_mesh.R
+++ figures/figure_01_mesh.R
@@ -1,10 +1,12 @@
 library(sf)
-#library(inlabru)
-#library(INLA)
+library(inlabru)
+library(INLA)
 library(dplyr)
-#library(fmesher)
+# library(fmesher)
 library(lubridate)
-library(cowplot)
+library(patchwork)
+library(ggplot2)
+library(viridis)
 # https://www.maths.ed.ac.uk/~flindgre/2018/07/22/spatially-varying-mesh-quality/
 
 haul <- readRDS("data/haul_cleaned.rds")
@@ -16,7 +18,7 @@ haul <- dplyr::filter(haul, year == 2018)
 coordinates(haul) <- c("X", "Y")
 
 # create boundary, same for all meshes
-boundary <- inla.nonconvex.hull(coordinates(haul),
+boundary <- inla.nonconvex.hull(sp::coordinates(haul),
   convex = -0.05
 )
 
@@ -44,35 +46,18 @@ plot_df <- data.frame(coordinates(haul))
 
 p2 <- ggplot() +
   gg(mesh2) +
-  theme_bw() +
+  ggsidekick::theme_sleek() +
   xlab("Eastings") +
   ylab("Northings") +
-  geom_point(data = plot_df, aes(X, Y), alpha = 0.3, col = viridis(1), size = 0.4) + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        plot.margin = margin(t = 5, r = 6, b = 20, l = 20))
+  geom_point(data = plot_df, aes(X, Y), alpha = 0.3, col = viridis(1), size = 0.4)
 
 p1 <- ggplot() +
   gg(mesh1) +
-  theme_bw() +
+  ggsidekick::theme_sleek() +
   xlab("Eastings") +
   ylab("Northings") +
-  geom_point(data = plot_df, aes(X, Y), alpha = 0.3, col = viridis(1), size = 0.4) + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        plot.margin = margin(t = 5, r = 6, b = 20, l = 20))
+  geom_point(data = plot_df, aes(X, Y), alpha = 0.3, col = viridis(1), size = 0.4)
 
-combined_plot <- cowplot::plot_grid(
-  p2, p1, 
-  align = "hv",
-  axis = "tblr",
-  nrow=1
-)
-
-final_plot <- ggdraw() + 
-  draw_plot(combined_plot, 0, 0, 1, 1) + 
-  draw_label("Eastings", x = 0.5, y = -0.05, vjust = -3, size=12) + 
-  draw_label("Northings", x = 0, y = 0, angle = 90, vjust = 1.5, hjust=-3.7, size=12)
-  
-ggsave(final_plot, filename = "figures/Figure_01.png", width=7, height = 6)
-
+p1 + p2 + plot_layout(ncol = 2, axes = "collect", axis_titles = "collect")
+ggsave("figures/Figure_01.png", width = 7, height = 6)
+ggsave("figures/Figure_01.pdf", width = 7, height = 6)
